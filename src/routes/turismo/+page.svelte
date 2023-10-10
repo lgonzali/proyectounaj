90bfbdaf9e6d3bddedf5fcb4dcd3c0653f0725c0
Carrera Pablo <ulisescarrera20@mail.com>
2023-10-10 15:57:04 -0300

    paginas añadidas

--- src/routes/turismo/+page.svelte
+++ src/routes/turismo/+page.svelte
@@ -10,7 +10,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
    <!-- site metas -->
-   <title>ARGENTINA</title>
+   <title>Services</title>
    <meta name="keywords" content="">
    <meta name="description" content="">
    <meta name="author" content="">
@@ -72,29 +72,14 @@
             <div class="menu_main">
                <ul>
                   <li class="active"><a href="hogar.html">Hogar</a></li>
-                  <li><a href="nosotros.html">Sobre nosotros</a></li>
+                  <li><a href="nosotros.html">Sobre Nosotros</a></li>
                   <li><a href="turismo.html">Turismo</a></li>
                   <li><a href="blog.html">Blog</a></li>
-                  <li><a href="sesion.html">Sesion</a></li>
+                  <li><a href="sesion.html">Inicia Sesion</a></li>
                </ul>
             </div>
          </div>
       </div>
-      <!-- banner section start -->
-      <div class="banner_section layout_padding">
-         <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-               <div class="carousel-item active">
-                  <div class="container">
-                     <h1 class="banner_taital">Turismo</h1>
-                     <p class="banner_text">info.</p>
-                     <div class="read_bt"><a href="#">Empieza</a></div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- banner section end -->
    </div>
    <!-- header section end -->
    <!-- services section start -->
@@ -121,122 +106,12 @@
       </div>
    </div>
    <!-- services section end -->
-   <!-- about section start -->
-   <div class="about_section layout_padding">
-      <div class="container-fluid">
-         <div class="row">
-            <div class="col-md-6">
-               <div class="about_taital_main">
-                  <h1 class="about_taital">Sobre Nosotros</h1>
-                  <p class="about_text">info.</p>
-                  <div class="readmore_bt"><a href="#">Inicio</a></div>
-               </div>
-            </div>
-            <div class="col-md-6 padding_right_0">
-               <div><img src="images/about-img.png" class="about_img"></div>
-            </div>
-         </div>
-      </div>
-   </div>
-   <!-- about section end -->
-   <!-- blog section start -->
-   <div class="blog_section layout_padding">
-      <div class="container">
-         <h1 class="blog_taital">Ve Nuestro Video</h1>
-         <p class="blog_text">info.</p>
-         <div class="play_icon_main">
-            <div class="play_icon"><a href="#"><img src="images/play-icon.png"></a></div>
-         </div>
-      </div>
-   </div>
-   <!-- blog section end -->
-   <!-- client section start -->
-   <div class="client_section layout_padding">
-      <div class="container">
-         <h1 class="client_taital">Reseñas</h1>
-         <div class="client_section_2">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-               <ol class="carousel-indicators">
-                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-               </ol>
-               <div class="carousel-inner">
-                  <div class="carousel-item active">
-                     <div class="client_main">
-                        <div class="box_left">
-                           <p class="lorem_text">info.</p>
-                        </div>
-                        <div class="box_right">
-                           <div class="client_taital_left">
-                              <div class="client_img"><img src="images/client-img.png"></div>
-                              <div class="quick_icon"><img src="images/quick-icon.png"></div>
-                           </div>
-                           <div class="client_taital_right">
-                              <h4 class="client_name">cliente</h4>
-                              <p class="customer_text">reseña</p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="carousel-item">
-                     <div class="client_main">
-                        <div class="box_left">
-                           <p class="lorem_text">info.</p>
-                        </div>
-                        <div class="box_right">
-                           <div class="client_taital_left">
-                              <div class="client_img"><img src="images/client-img.png"></div>
-                              <div class="quick_icon"><img src="images/quick-icon.png"></div>
-                           </div>
-                           <div class="client_taital_right">
-                              <h4 class="client_name">cliente</h4>
-                              <p class="customer_text">reseña</p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="carousel-item">
-                     <div class="client_main">
-                        <div class="box_left">
-                           <p class="lorem_text">info.</p>
-                        </div>
-                        <div class="box_right">
-                           <div class="client_taital_left">
-                              <div class="client_img"><img src="images/client-img.png"></div>
-                              <div class="quick_icon"><img src="images/quick-icon.png"></div>
-                           </div>
-                           <div class="client_taital_right">
-                              <h4 class="client_name">cliente</h4>
-                              <p class="customer_text">reseña</p>
-                           </div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-   </div>
-   <!-- client section start -->
-   <!-- choose section start -->
-   <div class="choose_section layout_padding">
-      <div class="container">
-         <h1 class="choose_taital">Por Que Elegirnos</h1>
-         <p class="choose_text">info.</p>
-         <div class="newsletter_box">
-            <h1 class="let_text">Empieza tu Propia Reseñas</h1>
-            <div class="getquote_bt"><a href="#">Empieza</a></div>
-         </div>
-      </div>
-   </div>
-   <!-- choose section end -->
    <!-- footer section start -->
    <div class="footer_section layout_padding">
       <div class="container">
          <div class="input_btn_main">
             <input type="text" class="mail_text" placeholder="Enter your email" name="Enter your email">
-            <div class="subscribe_bt"><a href="#">Inicia Sesion</a></div>
+            <div class="subscribe_bt"><a href="#">Subscribe</a></div>
          </div>
          <div class="location_main">
             <div class="call_text"><img src="images/call-icon.png"></div>
@@ -258,7 +133,7 @@
    <!-- copyright section start -->
    <div class="copyright_section">
       <div class="container">
-         <p class="copyright_text">2023 All Rights Reserved. Design by UNAJ</p>
+         <p class="copyright_text">2020 All Rights Reserved. Design by UNAJ></p>
       </div>
    </div>
    <!-- copyright section end -->
